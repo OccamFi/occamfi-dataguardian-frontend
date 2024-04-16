83ded59b4b95bfa11e852dba0c99dcc2e9060f03
alex-abanin <aa@occam.fi>
2024-04-16 15:24:22 +0300

    fallback in dev mode

--- readme.md
+++
@@ -1,5 +0,0 @@
-# react-starter
-
-Стартовый шаблон для создания приложений на React с Typescript и Vite, с подключенным TailwindCSS, ESLint и Prettier.
-
-Если нужны полифилы для модулей node, например, fs, buffer, process, то можно подключить [vite-plugin-node-polyfills](https://github.com/davidmyersdev/vite-plugin-node-polyfills).
--- src/shared/providers/holder-commitment-guard/holder-commitment-guard.tsx
+++ src/shared/providers/holder-commitment-guard/holder-commitment-guard.tsx
@@ -1,6 +1,8 @@
 import { useEffect } from "react";
 import { Navigate, Outlet, useSearchParams } from "react-router-dom";
 
+// http://localhost:5173/?holderCommitment=16607625577278438594767804415218927671757671726577118445895787921662140036661&encryptionPubKey=gkUeDzcB1eGHlTzX8V%2Bh8kwo28d8h799SUAhsaLuFyA%3D
+
 export const HolderCommitmentGuard = () => {
   const [searchParams] = useSearchParams();
   const holderCommitmentParam = searchParams.get("holderCommitment");
@@ -20,14 +22,22 @@ export const HolderCommitmentGuard = () => {
   const resultHolderEncryptionPubKey =
     savedEncryptionPubKey || encryptionPubKeyParam;
 
-  if (!resultHolderCommitment || !resultHolderEncryptionPubKey)
+  if (
+    import.meta.env.PROD &&
+    (!resultHolderCommitment || !resultHolderEncryptionPubKey)
+  )
     return <Navigate to="/no-holder-commitment" />;
 
   return (
     <Outlet
       context={{
-        holderCommitment: resultHolderCommitment,
-        encryptionPubKey: resultHolderEncryptionPubKey,
+        // TODO: remove hardcoded values before release
+        holderCommitment:
+          resultHolderCommitment ??
+          "16607625577278438594767804415218927671757671726577118445895787921662140036661",
+        encryptionPubKey:
+          resultHolderEncryptionPubKey ??
+          "gkUeDzcB1eGHlTzX8V%2Bh8kwo28d8h799SUAhsaLuFyA%3D",
       }}
     />
   );
